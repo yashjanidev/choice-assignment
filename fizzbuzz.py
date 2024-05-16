7e94972a6f2db3af377c6d01532a16cb43f7e389
yashjanidev <99663141+yashjanidev@users.noreply.github.com>
2024-05-16 15:09:33 +0530

    Create fizzbuzz.py

---
+++ fizzbuzz.py
@@ -0,0 +1,14 @@
+number = 100
+
+def fizzbuzz(number):
+    for i in range(0, number+1):
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
+
+fizzbuzz(number)
